ec2eb7c6d5a597e1bc1d989f1712751e64f510e0
Amruth <amruth1908@gmail.com>
2024-04-24 17:29:05 +0530

    added some routes

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,12 +1,33 @@
 import { useState } from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Login from './pages/login'
+import Signup from './pages/signup'
+import Dashboard from './pages/dashboard'
+import NewQuiz from './pages/newquiz'
+import Pokedex from './pages/pokedex'
+import Home from './pages/home'
+import UserPage from './pages/UserPage'
+import PlayQuiz from './pages/PlayQuiz'
+import Navbar from './pages/navbar'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='p-10'>hi</h1>
+    <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<Navbar/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/Dashboard' element={<Dashboard/>}/>
+        <Route path='/NewQuiz' element={<NewQuiz/>}/>
+        <Route path='/Pokedex' element={<Pokedex/>}/>
+        <Route path='/Home' element={<Home/>}/>
+        <Route path='/Play' element={<PlayQuiz/>}/>
+        <Route path='/User' element={<UserPage/>}/>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
---
+++ reactapp/src/pages/navbar.jsx
@@ -0,0 +1,12 @@
+import { React } from 'react'
+
+
+export default function Navbar() {
+  return (
+    <>
+      <h1>Navbar</h1>
+    </>
+  )
+}
+
+ 
