6c6057167da3270a7bb58932369496026ae3f88f
Amruth <amruth1908@gmail.com>
2024-05-08 22:24:46 +0530

    slight changes

--- reactapp/src/pages/QuizCardFroHome.jsx
+++ reactapp/src/pages/QuizCardFroHome.jsx
@@ -54,7 +54,7 @@ export default function QuizCard({doc}) {
         <span>{doc.data.date.toDate().toLocaleString()}</span>
       </div>
     </div>
-    <button type='button' onClick={()=>navigate(`/Play/:${doc.id}`)} class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2">
+    <button type='button' onClick={()=>navigate(`/Play/${doc.id}`)} class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2">
       Play Quiz
     </button>
   </div>
